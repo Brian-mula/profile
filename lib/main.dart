81ca9ac4d1cf987b17506bd6714777ebfac87321
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-15 12:29:13 -0700

    successfully connected to firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:payroll/connection.dart';
+import 'package:payroll/firebase_options.dart';
 import 'package:payroll/views/auth/login.dart';
 import 'package:payroll/views/auth/register.dart';
 import 'package:payroll/views/home/homepage.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
