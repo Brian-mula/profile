2c310982c34eb8af09ec99f1ad7fdfbf60358524
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-20 11:13:45 -0700

    added department provider

---
+++ lib/providers/departments_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:payroll/database/department_db.dart';
+import 'package:payroll/models/department_model.dart';
+
+final departmentsProvider = Provider<DepartmentController>((ref) {
+  return DepartmentController();
+});
+
+final allDepartments = StreamProvider<List<DepartmentModel>>((ref) {
+  return ref.read(departmentsProvider).allDepartments();
+});
