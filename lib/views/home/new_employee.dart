778616789ae6b3d57097510297a22b4f316e7d43
Brian-mula <mulatiwangilabrian@gmail.com>
2023-04-23 11:25:46 -0700

    implemented department dropdown

--- lib/views/home/new_employee.dart
+++ lib/views/home/new_employee.dart
@@ -1,6 +1,8 @@
 import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:payroll/providers/departments_provider.dart';
 
 class NewEmployee extends ConsumerStatefulWidget {
   const NewEmployee({super.key});
@@ -11,6 +13,8 @@ class NewEmployee extends ConsumerStatefulWidget {
 
 class _NewEmployeeState extends ConsumerState<NewEmployee> {
   final _formKey = GlobalKey<FormState>();
+  String? selectedDep;
+
   @override
   Widget build(BuildContext context) {
     TextEditingController fullname = TextEditingController();
@@ -23,155 +27,213 @@ class _NewEmployeeState extends ConsumerState<NewEmployee> {
     ThemeData theme = Theme.of(context);
 
     List<String> categories = ["Full time", "Part time", "Intern"];
+    final departments = ref.watch(allDepartmentsProvider);
 
     String? selectedCat;
     return Scaffold(
       body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Container(
-          padding: const EdgeInsets.only(top: 80, left: 10, right: 10),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "e",
-                    style: theme.textTheme.headlineLarge!
-                        .copyWith(color: Colors.orange.shade600),
-                  ),
-                  Text(
-                    "payroll",
-                    style: theme.textTheme.headlineLarge!
-                        .copyWith(color: Colors.black87),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Text(
-                "Provide employee details",
-                style: theme.textTheme.bodyLarge,
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        controller: fullname,
-                        decoration: fieldDecoration("Full name"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: idNumber,
-                        keyboardType: TextInputType.number,
-                        decoration: fieldDecoration("ID number"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: phone,
-                        keyboardType: TextInputType.phone,
-                        decoration: fieldDecoration("Phone"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: jobNumber,
-                        keyboardType: TextInputType.text,
-                        decoration: fieldDecoration("Job Number"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: department,
-                        keyboardType: TextInputType.text,
-                        decoration: fieldDecoration("Department"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      TextFormField(
-                        controller: salary,
-                        keyboardType: TextInputType.number,
-                        decoration: fieldDecoration("Salary"),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      DropdownButtonFormField2(
-                        decoration: InputDecoration(
-                            isDense: true,
-                            contentPadding: EdgeInsets.zero,
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(6)),
-                            enabledBorder: OutlineInputBorder(
-                                borderSide:
-                                    BorderSide(color: Colors.orange.shade600))),
-                        isExpanded: true,
-                        hint: Text(
-                          "Select category",
-                          style: theme.textTheme.bodyMedium!
-                              .copyWith(color: Colors.orange.shade600),
+          physics: const BouncingScrollPhysics(),
+          child: departments.when(
+              data: (departmentData) => Container(
+                    padding:
+                        const EdgeInsets.only(top: 80, left: 10, right: 10),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "e",
+                              style: theme.textTheme.headlineLarge!
+                                  .copyWith(color: Colors.orange.shade600),
+                            ),
+                            Text(
+                              "payroll",
+                              style: theme.textTheme.headlineLarge!
+                                  .copyWith(color: Colors.black87),
+                            )
+                          ],
                         ),
-                        items: categories
-                            .map((cat) => DropdownMenuItem<String>(
-                                  value: cat,
-                                  child: Text(
-                                    cat,
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Text(
+                          "Provide employee details",
+                          style: theme.textTheme.bodyLarge,
+                        ),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                TextFormField(
+                                  controller: fullname,
+                                  decoration: fieldDecoration("Full name"),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                TextFormField(
+                                  controller: idNumber,
+                                  keyboardType: TextInputType.number,
+                                  decoration: fieldDecoration("ID number"),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                TextFormField(
+                                  controller: phone,
+                                  keyboardType: TextInputType.phone,
+                                  decoration: fieldDecoration("Phone"),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                TextFormField(
+                                  controller: jobNumber,
+                                  keyboardType: TextInputType.text,
+                                  decoration: fieldDecoration("Job Number"),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                DropdownButtonFormField2(
+                                  decoration: InputDecoration(
+                                      isDense: true,
+                                      contentPadding: EdgeInsets.zero,
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Colors.orange.shade600))),
+                                  isExpanded: true,
+                                  hint: Text(
+                                    "Select Department",
                                     style: theme.textTheme.bodyMedium!.copyWith(
                                         color: Colors.orange.shade600),
                                   ),
-                                ))
-                            .toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            selectedCat = value;
-                          });
-                        },
-                        dropdownStyleData: DropdownStyleData(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(6))),
-                        buttonStyleData: ButtonStyleData(
-                            height: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(6),
-                            )),
-                        iconStyleData: IconStyleData(
-                            icon: Icon(
-                          Icons.arrow_drop_down,
-                          size: 30,
-                          color: Colors.orange.shade600,
-                        )),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SizedBox(
-                        width: 300,
-                        child: ElevatedButton(
-                            style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.orange.shade600),
-                                elevation: MaterialStateProperty.all(0.0)),
-                            onPressed: () {},
-                            child: const Text("Add Employee")),
-                      )
-                    ],
-                  ))
-            ],
-          ),
-        ),
-      ),
+                                  items: departmentData
+                                      .map((dept) => DropdownMenuItem<String>(
+                                            value: dept.name,
+                                            child: Text(
+                                              dept.name!,
+                                              style: theme.textTheme.bodyMedium!
+                                                  .copyWith(
+                                                      color: Colors
+                                                          .orange.shade600),
+                                            ),
+                                          ))
+                                      .toList(),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedDep = value;
+                                    });
+                                  },
+                                  dropdownStyleData: DropdownStyleData(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(6))),
+                                  buttonStyleData: ButtonStyleData(
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(6),
+                                      )),
+                                  iconStyleData: IconStyleData(
+                                      icon: Icon(
+                                    Icons.arrow_drop_down,
+                                    size: 30,
+                                    color: Colors.orange.shade600,
+                                  )),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                TextFormField(
+                                  controller: salary,
+                                  keyboardType: TextInputType.number,
+                                  decoration: fieldDecoration("Salary"),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                DropdownButtonFormField2(
+                                  decoration: InputDecoration(
+                                      isDense: true,
+                                      contentPadding: EdgeInsets.zero,
+                                      border: OutlineInputBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(6)),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Colors.orange.shade600))),
+                                  isExpanded: true,
+                                  hint: Text(
+                                    "Select category",
+                                    style: theme.textTheme.bodyMedium!.copyWith(
+                                        color: Colors.orange.shade600),
+                                  ),
+                                  items: categories
+                                      .map((cat) => DropdownMenuItem<String>(
+                                            value: cat,
+                                            child: Text(
+                                              cat,
+                                              style: theme.textTheme.bodyMedium!
+                                                  .copyWith(
+                                                      color: Colors
+                                                          .orange.shade600),
+                                            ),
+                                          ))
+                                      .toList(),
+                                  onChanged: (value) {
+                                    setState(() {
+                                      selectedCat = value;
+                                    });
+                                  },
+                                  dropdownStyleData: DropdownStyleData(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(6))),
+                                  buttonStyleData: ButtonStyleData(
+                                      height: 50,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(6),
+                                      )),
+                                  iconStyleData: IconStyleData(
+                                      icon: Icon(
+                                    Icons.arrow_drop_down,
+                                    size: 30,
+                                    color: Colors.orange.shade600,
+                                  )),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                SizedBox(
+                                  width: 300,
+                                  child: ElevatedButton(
+                                      style: ButtonStyle(
+                                          backgroundColor:
+                                              MaterialStateProperty.all(
+                                                  Colors.orange.shade600),
+                                          elevation:
+                                              MaterialStateProperty.all(0.0)),
+                                      onPressed: () {},
+                                      child: const Text("Add Employee")),
+                                )
+                              ],
+                            ))
+                      ],
+                    ),
+                  ),
+              error: (err, stackTrace) => Text(err.toString()),
+              loading: () => Center(
+                  child: LoadingAnimationWidget.flickr(
+                      leftDotColor: const Color(0xFF0063DC),
+                      rightDotColor: const Color(0xFFFF0084),
+                      size: 100)))),
     );
   }
 
